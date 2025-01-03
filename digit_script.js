89b56b3e98e4c47b2d95c4005ec0a568a1db100a
Sourav Routray <souravroutray2016@gmail.com>
2025-01-03 12:56:37 +0530

    added date modification according to month

--- digit_script.js
+++ digit_script.js
@@ -1,130 +1,87 @@
 // digit
 const current_date_object = new Date();
-let birth_date = document.getElementById("birth_date");
-let birth_month = document.getElementById("birth_month");
-let birth_year = document.getElementById("birth_year");
-let current_date = document.getElementById("current_date");
-let current_month = document.getElementById("current_month");
-let current_year = document.getElementById("current_year");
+const birth_month = document.getElementById("birth_month");
+const birth_year = document.getElementById("birth_year");
+const current_date = document.getElementById("current_date");
+const birth_date = document.getElementById("birth_date");
+const current_month = document.getElementById("current_month");
+const current_year = document.getElementById("current_year");
 
 let currentDate = current_date_object.getDate();
 if (currentDate / 10 < 1) current_date.value = `0${currentDate}`;
 else current_date.value = `${currentDate}`;
+console.log(typeof currentDate);
 
 let currentMonth = current_date_object.getMonth() + 1;
 if (currentMonth / 10 < 1) current_month.value = `0${currentMonth}`;
 else current_month.value = `${currentMonth}`;
+console.log(typeof currentMonth);
 
 let currentYear = current_date_object.getFullYear();
 current_year.value = `${currentYear}`;
+console.log(typeof currentDate);
 
 function get_current_date_info() {
-  currentDate = current_date.value;
+  currentDate = parseInt(current_date.value);
   if (currentDate / 10 < 1) current_date.value = `0${currentDate}`;
 }
 function get_current_month_info() {
-  get_current_month = current_month.value;
+  currentMonth = parseInt(current_month.value);
   if (currentMonth / 10 < 1) current_month.value = `0${currentMonth}`;
+  date_fix(current_date, currentDate, currentMonth, currentYear);
 }
 function get_current_year_info() {
-  currentYear = current_year.value;
+  currentYear = parseInt(current_year.value);
+  date_fix(current_date, currentDate, currentMonth, currentYear);
 }
 
 let birthDate, birthMonth, birthYear;
 
 function get_birth_date_info() {
-  birthDate = birth_date.value;
+  birthDate = parseInt(birth_date.value);
   if (birthDate / 10 < 1) birth_date.value = `0${birthDate}`;
 }
 function get_birth_month_info() {
-  birthMonth = birth_month.value;
+  birthMonth = parseInt(birth_month.value);
   if (birthMonth / 10 < 1) birth_month.value = `0${birthMonth}`;
+  date_fix(birth_date, birthDate, birthMonth, birthYear);
 }
 function get_birth_year_info() {
-  birthYear = birth_year.value;
+  birthYear = parseInt(birth_year.value);
+  date_fix(birth_date, birthDate, birthMonth, birthYear);
 }
 
-// function age_calculation() {
-//   const calculatedAge = document.getElementById("calculatedAge");
-//   if (birth.value === "" || current.value === "") {
-//     calculatedAge.innerHTML = "Enter Birth Date Above...";
-//     return;
-//   }
-
-//   if (
-//     birth_year === current_year &&
-//     birth_month === current_month &&
-//     birthDate > currentDate
-//   ) {
-//     calculatedAge.innerHTML = "INVALID DATA GIVEN";
-//     return;
-//   }
-//   if (birth_year === current_year && birth_month > current_month) {
-//     calculatedAge.innerHTML = "INVALID DATA GIVEN";
-//     return;
-//   }
-//   if (birth_year > current_year) {
-//     calculatedAge.innerHTML = "INVALID DATA GIVEN";
-//     return;
-//   }
-
-//   let ageYear, ageMonth, ageDay;
-//   ageYear = current_year - birth_year;
-
-//   if (birth_month > current_month) {
-//     ageYear -= 1;
-//     ageMonth = 12 - birth_month + current_month;
-//   } else ageMonth = current_month - birth_month;
-
-//   if (birthDate > currentDate) {
-//     ageMonth -= 1;
-//     if (ageMonth === -1) {
-//       ageMonth = 11;
-//       ageYear -= 1;
-//     }
-//     switch (current_month) {
-//       case 1:
-//       case 2:
-//       case 4:
-//       case 6:
-//       case 8:
-//       case 9:
-//       case 11:
-//         ageDay = 31 - birthDate + currentDate;
-//         break;
-//       case 3:
-//         if (
-//           (current_year % 4 === 0 && current_year % 400 !== 0) ||
-//           current_year % 400 === 0
-//         ) {
-//           if (29 - birthDate < 0) ageDay = currentDate;
-//           else ageDay = 29 - birthDate + currentDate;
-//         } else ageDay = 28 - birthDate + currentDate;
-//         break;
-//       case 5:
-//       case 7:
-//       case 10:
-//       case 12:
-//         if (30 - birthDate < 0) ageDay = currentDate;
-//         else ageDay = 30 - birthDate + currentDate;
-//         break;
-//     }
-//   } else ageDay = currentDate - birthDate;
-
-//   if (ageYear === 0 && ageMonth === 0 && ageDay === 0)
-//     calculatedAge.innerHTML = "Happy Born-Day!!!";
-//   else if (ageYear === 0 && ageMonth === 0)
-//     calculatedAge.innerHTML = `You're ${ageDay}Days Old.`;
-//   else if (ageYear === 0 && ageDay === 0)
-//     calculatedAge.innerHTML = `You're ${ageMonth}Months Old.`;
-//   else if (ageMonth === 0 && ageDay === 0)
-//     calculatedAge.innerHTML = `You're ${ageYear}Years Old.`;
-//   else if (ageYear === 0)
-//     calculatedAge.innerHTML = `You're ${ageMonth}Months, ${ageDay}Days Old.`;
-//   else if (ageMonth === 0)
-//     calculatedAge.innerHTML = `You're ${ageYear}Years, ${ageDay}Days Old.`;
-//   else if (ageDay === 0)
-//     calculatedAge.innerHTML = `You're ${ageYear}Years, ${ageMonth}Months Old.`;
-//   else
-//     calculatedAge.innerHTML = `You're ${ageYear}Years, ${ageMonth}Months, ${ageDay}Days Old.`;
-// }
+function date_fix(date_id, date, month, year) {
+  switch (month) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      date_id.max = "31";
+      break;
+    case 2:
+      if ((year % 4 === 0 && year % 400 !== 0) || year % 400 === 0) {
+        if (date > 29) {
+          date = 29;
+          date_id.value = date;
+        }
+        date_id.max = "29";
+      } else {
+        if (date > 28) {
+          date = 28;
+          date_id.value = date;
+        }
+        date_id.max = "28";
+      }
+      break;
+    default:
+      if (date > 30) {
+        date = 30;
+        date_id.value = date;
+      }
+      date_id.max = "30";
+  }
+}
